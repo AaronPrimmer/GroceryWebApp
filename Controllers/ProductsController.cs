ccb277a05af77d10fdab3c62fe35a8551da2d42e
Aaron Primmer <aaronmprimmer@gmail.com>
2026-04-14 06:53:54 -0400

    Added Product Add Functionality

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = ProductRepository.GetProducts(true);
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var productViewModel = new ProductViewModel
+            {
+                Categories = CategoriesRepository.GetAllCategories()
+            };
+
+            return View(productViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductRepository.AddProduct(productViewModel.Product);
+                return RedirectToAction("Index");
+            }
+
+            productViewModel.Categories = CategoriesRepository.GetAllCategories();
+            return View(productViewModel);
+        }
+    }
+}
